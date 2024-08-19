3df4fc23b21dcb70217663ad39f65620cbafc724
Mad-ArrA <arkadi8377@gmail.com>
2024-08-19 23:23:56 +0300

    UPD

---
+++ TestAIO_with_Profile.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+import time
+import allure
+
+
+@pytest.fixture
+def driver():
+    with allure.step("Инициализация драйвера браузера"):
+        options_chrome  = webdriver.ChromeOptions()
+        # Указываем путь к профилю пользователя
+        options_chrome.add_argument('user-data-dir=C:\\Users\\ArrA\\AppData\\Local\\Google\\Chrome\\User Data')
+        # Инициализируем драйвер с указанными опциями
+        driver = webdriver.Chrome(options=options_chrome)
+        driver.get("https://aio-pro-frontend.vercel.app/connect")
+        yield driver
+        driver.quit()
+
+@allure.feature('Connect Wallet')
+@allure.story('MetaMask Wallet Connection')
+# @pytest.mark.skip(reason="Тест временно отключен")
+def test_click_login(driver):
+
+    with allure.step("Ввод пароля и авторизация"):
+        password_input = WebDriverWait(driver, 10).until(
+            ec.presence_of_element_located((By.CLASS_NAME, "field__input"))
+        )
+        password_input.click()
+
+    time.sleep(10)
+
+    button_login = WebDriverWait(driver, 10).until(
+        ec.element_to_be_clickable((By.CLASS_NAME, "actions__submit"))
+    )
+    button_login.click()
+
+    with allure.step("Проверка успешной авторизации"):
+        assert WebDriverWait(driver, 10).until(
+            ec.url_to_be("https://aio-pro-frontend.vercel.app/accounts/evm/wallets")
+        )
+
--- TestAIO_without_Profile.py
+++ TestAIO_without_Profile.py
@@ -5,24 +5,15 @@ from selenium.webdriver.support import expected_conditions as ec
 import pytest
 import time
 import allure
-import subprocess
 
-# def run_tests():
-#     # Запуск pytest с указанием директории для Allure отчетов
-#     subprocess.run(["pytest", "--alluredir=allure-results"])
-#
-# def generate_report():
-#     # Запуск генерации Allure отчета
-#     subprocess.run(["allure", "serve", "allure-results"])
 
 @pytest.fixture
 def driver():
     with allure.step("Инициализация драйвера браузера"):
-        driver = webdriver.Chrome()  # Убедитесь, что chromedriver находится в PATH
+        driver = webdriver.Chrome()
     yield driver
     driver.quit()
-    # run_tests()
-    # generate_report()
+
 
 @allure.feature('Connect Wallet')
 @allure.story('MetaMask Wallet Connection')
@@ -66,18 +57,3 @@ def test_open_page(driver):
 
 
     # driver.save_screenshot("screenshot.png")
-
-
-# def test_element_presence(driver):
-#     driver.get("https://example.com")
-#     elem = driver.find_element(By.TAG_NAME, "h1")
-#     assert elem.text == "Example Domain"
-#
-#
-# def test_google_search(driver):
-#     driver.get('https://www.google.com')
-#     search_box = driver.find_element(By.NAME, 'q')
-#     search_box.send_keys('pytest')
-#     search_box.submit()
-#     assert 'pytest' in driver.title
-#     driver.save_screenshot("page.html")
