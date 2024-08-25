3b0c5e3fc9eec616d6f6cc27b19b93599bf1c981
Mad-ArrA <arkadi8377@gmail.com>
2024-08-25 19:38:33 +0300

    UPD

--- TestAIO_with_Profile.py
+++ TestAIO_with_Profile.py
@@ -7,7 +7,9 @@ import pytest
 import time
 import allure
 
+NAME_EXTENSION = "MetaMask"
 NAME_GROUP = "Test Group 2"
+PASSWORD_METAMASK = "arm121309"
 # Устанавливает область видимости фикстуры на модульный уровень.
 # Это означает, что браузер будет инициализироваться один раз для всех тестов в модуле и будет закрываться после завершения всех тестов.
 @pytest.fixture(scope="module")
@@ -19,14 +21,16 @@ def driver():
         # Инициализируем драйвер с указанными опциями
         driver = webdriver.Chrome(options=options_chrome)
         # driver.set_window_size("1936x1048")
-        driver.get("https://aio-pro-frontend.vercel.app/connect")
         yield driver
         driver.quit()
 
 @allure.feature('Connect Wallet')
 @allure.story('MetaMask/Rabby Wallet Connection')
 # @pytest.mark.skip(reason="Тест временно отключен")
-def test_click_login(driver):
+# @pytest.mark.dependency(name="connect_wallet")
+def test_click_connect(driver):
+    driver.get("https://aio-pro-frontend.vercel.app/connect")
+
     with allure.step("Открытие страницы и клик на Connect Wallet"):
         connect_wallet_button = WebDriverWait(driver, 10).until(
             ec.element_to_be_clickable((By.CLASS_NAME, "connect-wallet__open"))
@@ -41,8 +45,75 @@ def test_click_login(driver):
         if button_connect:
             button_connect[2].click()
 
-    time.sleep(10)  # Даем время на открытие окна
+    # Убедимся, что выбран MetaMask
+    #assert "connectors__connect" in driver.page_source
 
+    time.sleep(10)
+    # time.sleep(15)
+
+# подключаем MetaMask через расширение
+# Тест для подключения MetaMask через расширение
+# @allure.feature('Connect MetaMask')
+# @allure.story('MetaMask Extension Connection')
+# @pytest.mark.dependency(depends=["connect_wallet"])
+# def test_connect_metamask(driver):
+    original_window = driver.current_window_handle  # Сохраняем текущее окно
+
+    with allure.step("Открытие нового окна"):
+        driver.execute_script("window.open('');")  # Открываем новое окно
+        new_window = driver.window_handles[-1]  # Получаем дескриптор нового окна
+        driver.switch_to.window(new_window)  # Переключаемся на новое окно
+
+    # Добавление текста в отчет Allure
+    allure.attach(f"Открылось новое окно с дескриптором: {new_window}", "Открытие нового окна",allure.attachment_type.TEXT)
+
+    url = 'chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html'
+    driver.get(url)
+
+    time.sleep(5)
+
+    with allure.step("Ввод пароля Metamask"):
+        input_password_metamask = WebDriverWait(driver, 10).until(
+            ec.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="unlock-password"]'))
+        )
+        input_password_metamask.send_keys(PASSWORD_METAMASK)
+
+    # Добавление скриншота после ввода пароля
+    # allure.attach(driver.get_screenshot_as_png(), name="Скриншот после ввода пароля", attachment_type=allure.attachment_type.PNG)
+
+    with allure.step("Нажимаем Разблокировать"):
+        button_unblock = WebDriverWait(driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="unlock-submit"]'))
+        )
+        button_unblock.click()
+
+    # Добавление текста в отчет
+    allure.attach("Кнопка 'Разблокировать' была нажата", "Клик по кнопке 'Разблокировать'",allure.attachment_type.TEXT)
+
+    with allure.step("Нажимаем Далее"):
+        button_next = WebDriverWait(driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="page-container-footer-next"]'))
+        )
+        button_next.click()
+
+    with allure.step("Нажимаем Подтвердить"):
+        button_confirm = WebDriverWait(driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="page-container-footer-next"]'))
+        )
+        button_confirm.click()
+
+    # Добавление скриншота
+    # allure.attach(driver.get_screenshot_as_png(), name="Скриншот после нажатия 'Далее'",attachment_type=allure.attachment_type.PNG)
+
+    with allure.step("Закрываем новое окно и возвращаемся к исходному"):
+        driver.close()  # Закрываем новое окно
+        driver.switch_to.window(original_window)  # Возвращаемся к исходному окну
+        allure.attach("Возврат к исходному окну", "Закрытие нового окна", allure.attachment_type.TEXT)
+
+@allure.feature('Login')
+@allure.story('Login')
+# @pytest.mark.skip(reason="Тест временно отключен")
+def test_connect_metamask(driver):
     with allure.step("Ввод пароля и авторизация"):
         password_input = WebDriverWait(driver, 10).until(
             ec.presence_of_element_located((By.CLASS_NAME, "field__input"))
@@ -76,6 +147,7 @@ def test_click_login(driver):
 
 @allure.feature('Create Group')
 @allure.story('Create group account manager')
+@pytest.mark.skip(reason="Тест временно отключен")
 def test_create_group_account_manager(driver):
     with allure.step("Добавление группы"):
         add_group_accmanager = WebDriverWait(driver, 10).until(
@@ -89,12 +161,15 @@ def test_create_group_account_manager(driver):
         )
         name_group_input.send_keys(NAME_GROUP)
 
+    allure.attach(driver.get_screenshot_as_png(), name="Скриншот после ввода названия", attachment_type=allure.attachment_type.PNG)
+
     with allure.step("Кнопка Create"):
         button_create_group = WebDriverWait(driver, 10).until(
             ec.element_to_be_clickable((By.CLASS_NAME, "left-side__ok"))
         )
         button_create_group.click()
 
+    allure.attach(driver.get_screenshot_as_png(), name="Скриншот после нажатия на кнопку Create", attachment_type=allure.attachment_type.PNG)
     time.sleep(10)
 
     with allure.step("Проверка успешного добавления группы"):
